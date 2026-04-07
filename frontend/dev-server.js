1c99a94350735dcef2ba72729a9a52b980fa644e
Maxmud Yusupov <maxmudyusupov11@gmail.com>
2026-04-07 16:55:30 +0500

    Add Next.js frontend project config and package files

---
+++ frontend/dev-server.js
@@ -0,0 +1,3 @@
+// Wrapper to ensure node is in PATH for Turbopack child processes
+process.env.PATH = '/usr/local/bin:/opt/homebrew/bin:' + (process.env.PATH || '/usr/bin:/bin:/usr/sbin:/sbin');
+require('next/dist/bin/next');
---
+++ frontend/start-dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH="/usr/local/bin:/opt/homebrew/bin:$PATH"
+exec npm run dev
